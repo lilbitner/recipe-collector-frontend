505c1006a52530d87385e61c2bb5d0f9d950d798
github username <github email address>
2020-04-10 16:53:52 -0600

    Add card styling, ternaries for card attributes and loading gif

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const gcs = require('@google-cloud/storage')();
+const os = require('os');
+const path = require('path')
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.storage.object().onChange(event => {
+//     const object = event.data
+//     const bucket = object.bucket
+//     const contentType = object.contentType 
+//     const filePath = object.name;
+//     console.log('file change detected, function started')
+
+//     const destBucket = gcs.bucket(bucket)
+//     const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath))
+//     const metadata  = { contentType: contentType}
+//     return destBucket.file(filePath).download({
+//         destination: tmpFilePath
+//     }).then(() => {
+//         return destBucket.upload(tmpFilePath, {
+//             destination: 'renamed-' + path.basename(filePath)}
+//     })
+// })
+
+//     return; 
+// });
--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Styling/RecipeCard.css'
+import {Link} from 'react-router-dom'
 
 export default class RecipeCard extends React.Component{
     
@@ -8,10 +9,12 @@ export default class RecipeCard extends React.Component{
     render(){
         return(
             <div className='recipeCard'>
-                <h3>{this.props.title}</h3>
-                {/* <h4>{this.props.category}</h4> */}
-                <img id='recipeImage' src={this.props.image} />
-                <button>Delete Recipe</button>
+                {this.props.title.length !== 0 ? <h3>{this.props.title}</h3> : null }
+                {this.props.description.length !== 0 ? <h4>{this.props.description}</h4> : null }
+                {this.props.url.length !== 0 ? <Link>{this.props.url}</Link> : null }
+                {this.props.image.length !== 0 ? <img id='recipeImage' src={this.props.image} /> : null }
+                {/* <img id='recipeImage' src={this.props.image} /> */}
+                {/* <button>Delete Recipe</button> */}
             </div>
         )
     }
