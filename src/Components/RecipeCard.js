6f4a62a2591b1766d8b3969eac35c3ef92618c2d
github username <github email address>
2020-04-12 12:18:02 -0600

    Add delete button functionality and rotation of image button in recipe card component

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -8,25 +8,41 @@ import 'react-medium-image-zoom/dist/styles.css'
 export default class RecipeCard extends React.Component{
     
     state = {
-        recipeId: ''
+        recipeId: '',
+        rotation: 0
     }
     
     handleClick = (event) => {
-       console.log(event)
+        event.preventDefault()
+        console.log(event)
         this.props.deleteRecipe(this.props.id)
      }
+    handleRotation = (event) => {
+        let newRotation = this.state.rotation + 90;
+        if(newRotation >= 360){
+        newRotation =- 360;
+    }
+        this.setState({
+        rotation: newRotation,
+    })
+    }
 
 
     render(){
         return(
             <div className='recipeCard'>
                 {this.props.title.length !== 0 ? <h3>{this.props.title}</h3> : null }
-                {this.props.description.length !== 0 ? <h4>{this.props.description}</h4> : null }
+                {this.props.description.length !== 0 ? <h4 id='description'>{this.props.description}</h4> : null }
                 {this.props.url.length !== 0 ? <a href={this.props.url}>Recipe URL</a> : null }
-                {this.props.image.length !== 0 ? <Zoom> <img id='recipeImage' src={this.props.image} style={{width: '50%', height: '100%', marginLeft: '25%', marginTop: '0%'}}  /></Zoom> : null }
+                {this.props.image.length !== 0 ? <Zoom> <img id='recipeImage' src={this.props.image} style={{width: '50%',
+                     height: '100%', marginLeft: '25%', marginTop: '0%', transform: `rotate(${this.state.rotation}deg)`}}  /></Zoom> : null 
+                }
                 {/* {this.props.image.length !== 0 ? <img id='recipeImage' src={this.props.image} /> : null } */}
                 {/* <img id='recipeImage' src={this.props.image} /> */}
-                <button onClick={this.handleClick}>Delete Recipe</button>
+                <div className='buttonContainer'>
+                    <button id='deleteButton' onClick={this.handleRotation}>Rotate Image</button>
+                    <button id='deleteButton' onClick={this.handleClick}>Delete Recipe</button>
+                </div>
             </div>
         )
     }
